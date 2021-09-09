a57ff98ea18729fda27d1f628683bc2573f5f16f
Ariel Ebersberger <ariel@ebersberger.io>
2021-09-09 09:19:31 +0200

    TEST 4

--- src/main/java/com/pi4j/example/MinimalExample.java
+++ src/main/java/com/pi4j/example/MinimalExample.java
@@ -136,8 +136,7 @@ public class MinimalExample {
             if (led.equals(DigitalState.HIGH)) {
                 console.println("LED low");
                 led.low();
-                Thread.sleep(500)
-                
+                Thread.sleep(500);
             } else {
                 console.println("LED high");
                 led.high();
