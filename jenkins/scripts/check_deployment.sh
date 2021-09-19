2c3b4d1664a483d96ae1c78a1d7d1982614ffe80
Ariel Ebersberger <ariel@ebersberger.io>
2021-09-19 22:10:50 +0200

    MODIFY deployment script

--- jenkins/scripts/check_deployment.sh
+++ jenkins/scripts/check_deployment.sh
@@ -1,12 +1,24 @@
-DEP=$(sudo kubectl get deployments 2> /dev/null || true | grep "$1")
-POD=$(sudo kubectl get pods 2> /dev/null || true | grep "$1")
+#!/bin/sh
 
-if [  -n "$DEP" ]; then
+deployment_active () {
+  POD=$(sudo kubectl get pods 2> /dev/null | grep "$1")
+  if [ -n "$POD" ]
+  then
+    return 0
+  else 
+    return 1
+  fi 
+}
+
+if deployment_active
+then
+  # Delete current Deployment
   sudo kubectl delete deployment $1
-  echo 'Deleting...'
-  while [ -n "$POD" ]; do
-    echo 'Not deleted! Continue...'
+
+  # Check if deployment got deleted
+  while deployment_active
+  do
+    echo 'Waiting for termination to finish...'
     sleep 5
-    POD=$(sudo kubectl get pods 2> /dev/null || true | grep "$1")
   done
 fi
